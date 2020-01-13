e23703b693de03b71e0554c8a8d9bcd1a827f818
FarrandTom <33008723+FarrandTom@users.noreply.github.com>
2020-01-13 13:49:06 +0000

    Change mounting behaviour

--- scripts/s3fs.sh
+++ scripts/s3fs.sh
@@ -13,17 +13,18 @@ chmod 600 ${BASEDIR}/.passwd-s3fs
 
 # Need to get a copy over existing data from PAIV to the COS bucket.
 # Create a temporary mount, copy the data across, and then unmount.
-mkdir ${BASEDIR}/temp/
-chmod 755 ${BASEDIR}/temp/
-s3fs ${BUCKET_NAME} ${BASEDIR}/temp/ -o url=http://${PUBLIC_ENDPOINT} -o passwd_file=${BASEDIR}/.passwd-s3fs
-echo "Copying existing files to COS..."
-cp -r /opt/powerai-vision/volume/data/ ${BASEDIR}/temp/
-umount ${BASEDIR}/temp/
+# mkdir ${BASEDIR}/temp/
+# chmod 755 ${BASEDIR}/temp/
+# s3fs ${BUCKET_NAME} ${BASEDIR}/temp/ -o url=http://${PUBLIC_ENDPOINT} -o passwd_file=${BASEDIR}/.passwd-s3fs
+# echo "Copying existing files to COS..."
+# cp -r /opt/powerai-vision/volume/data/ ${BASEDIR}/temp/
+# umount ${BASEDIR}/temp/
 #fusermount -uz ./temp/
 
 # Now that we have a backup of existing files, we can overwrite and mount at the point where
 # PAIV will save logs and user data.
-echo "Linking to /opt/powerai-vision/volume/data/"
+echo "Mounting at /opt/powerai-vision/volume/data/"
+mkdir /opt/powerai-vision/volume/data/ -p 
 s3fs ${BUCKET_NAME} /opt/powerai-vision/volume/data/ -o url=http://${PUBLIC_ENDPOINT} -o passwd_file=${BASEDIR}/.passwd-s3fs -o nonempty
 
 # Cleanup
