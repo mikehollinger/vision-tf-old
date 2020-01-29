79ca56014cd0947ff71167024d02d31210ce3cbc
Srijith Prabhu <snprabhu@us.ibm.com>
2020-01-29 10:50:58 -0600

    Adding if statement to see if the driver is already there.

--- scripts/install_gpu_drivers.sh
+++ scripts/install_gpu_drivers.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -xe
 # Install NVIDIA Driver
-arch='ppc64le'
-version='418.116.00'
-driver_version=${arch}-${version}
-run_name=NVIDIA-Linux-${driver_version}.run
-sudo wget http://us.download.nvidia.com/tesla/${version}/${run_name}
-chmod +x ${run_name}
-./${run_name} -a -s
-nvidia-smi
+modinfo nvidia && which nvidia-smi
+has_gpu_driver=$?
+if !has_gpu_driver; then
+  arch='ppc64le'
+  version='418.116.00'
+  driver_version=${arch}-${version}
+  run_name=NVIDIA-Linux-${driver_version}.run
+  sudo wget http://us.download.nvidia.com/tesla/${version}/${run_name}
+  chmod +x ${run_name}
+  ./${run_name} -a -s
+  nvidia-smi
+fi
