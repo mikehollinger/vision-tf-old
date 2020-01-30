31f6982e4a01039cac4219ff80b55bd47be735fb
Srijith Prabhu <snprabhu@us.ibm.com>
2020-01-30 10:35:57 -0600

    removing the cuda install. Enabling nvidia-persistenced

--- scripts/install_gpu_drivers.sh
+++ scripts/install_gpu_drivers.sh
@@ -5,6 +5,7 @@ has_gpu_driver=$?
 if [ $has_gpu_driver -ne 0 ]; then
   # Install Nvidia
   apt-get -o Dpkg::Use-Pty=0 update -qq  || echo " RC${?} Got an error on update???"
+  # make and gcc required for Nvidia driver install
   apt-get -o Dpkg::Use-Pty=0 install -qq build-essential
   echo "Installing Nvidia drivers."
   arch='ppc64le'
@@ -15,15 +16,8 @@ if [ $has_gpu_driver -ne 0 ]; then
   sudo wget http://us.download.nvidia.com/tesla/${version}/${run_name}
   chmod +x ${run_name}
   ./${run_name} -a -s
+  systemctl enable nvidia-persistenced
   nvidia-smi
-
-  # Install CUDA
-  apt-get install linux-headers-$(uname -r)
-  dpkg -i cuda-repo-${distro}_${version}_${arch}.deb
-  apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${distro}/${arch}/7fa2af80.pub
-  apt-get update
-  apt-get install cuda
-
   # apt-get -o Dpkg::Use-Pty=0 remove -qq gcc make
 else
   echo "Nvidia drivers installed on machine already. Skipping install of drivers."
