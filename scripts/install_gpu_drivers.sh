26af9b4b8e771bd6f4b302698076016df614cc24
Srijith Prabhu <snprabhu@us.ibm.com>
2020-01-31 16:04:05 -0600

    Adding some clean up steps.

--- scripts/install_gpu_drivers.sh
+++ scripts/install_gpu_drivers.sh
@@ -19,9 +19,13 @@ if [ $has_gpu_driver -ne 0 ]; then
   # ./${run_name} -a -s
   deb_name=http://us.download.nvidia.com/tesla/${version}/nvidia-driver-local-repo-${distro}-${version}_1.0-1_${arch}.deb
   wget ${deb_name}
-  dpkg -i nvidia-driver-local-repo-ubuntu1804-418.*.deb
-  apt-get update
+  dpkg -i nvidia-driver-local-repo-${distro}-${version}.*.deb
+  apt-get -o Dpkg::Use-Pty=0 update -qq  || echo " RC${?} Got an error on update???"
   apt-get -o Dpkg::Use-Pty=0 install -qq nvidia-driver-418
+  # Purge the repo
+  dpkg -P `dpkg -l | grep nvidia-driver-local-repo-ubuntu | cut -d " " -f 3`
+  apt-get clean -y
+  rm nvidia-driver-local-repo-${distro}-${version}.*.deb
   systemctl daemon-reload
   systemctl enable nvidia-persistenced
   nvidia-smi
