6bc8e84354ff49fe90eeade1c624bbc643dd6d2a
Srijith Prabhu <snprabhu@us.ibm.com>
2020-01-31 16:21:28 -0600

    using the full name of the debian.

--- scripts/install_gpu_drivers.sh
+++ scripts/install_gpu_drivers.sh
@@ -17,7 +17,7 @@ if [ $has_gpu_driver -ne 0 ]; then
   # sudo wget http://us.download.nvidia.com/tesla/${version}/${run_name}
   # chmod +x ${run_name}
   # ./${run_name} -a -s
-  deb_name=http://us.download.nvidia.com/tesla/${version}/nvidia-driver-local-repo-${distro}-${version}_1.0-1_${arch}.deb
+  deb_name=http://us.download.nvidia.com/tesla/418.116.00/nvidia-driver-local-repo-ubuntu1804-418.116.00_1.0-1_ppc64el.deb
   wget ${deb_name}
   dpkg -i nvidia-driver-local-repo-${distro}-${version}.*.deb
   apt-get -o Dpkg::Use-Pty=0 update -qq  || echo " RC${?} Got an error on update???"
@@ -25,7 +25,7 @@ if [ $has_gpu_driver -ne 0 ]; then
   # Purge the repo
   dpkg -P `dpkg -l | grep nvidia-driver-local-repo-ubuntu | cut -d " " -f 3`
   apt-get clean -y
-  rm nvidia-driver-local-repo-${distro}-${version}.*.deb
+  rm nvidia-driver-local-repo-*.deb
   systemctl daemon-reload
   systemctl enable nvidia-persistenced
   nvidia-smi
